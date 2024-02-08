61d958169a0523bcbed48cba68c2a9fc8691cb21
Harsh <153886419+Harsh-nodejs@users.noreply.github.com>
2024-02-08 09:33:40 +0530

    Revert "nn"
    This reverts commit 80cd0d385241cdf37902f634bc736d224ad91a95.

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -86,20 +86,16 @@ exports.getAllArticle = catchAsync(async (req, res, next) => {
 exports.getArticle = catchAsync(async (req, res, next) => {
   // check ID for number or Name
   const articleOne = await Article.findById(req.params.id);
+  // Tour.findOne({ _id: req.params.id })
 
   if (!articleOne) {
     return next(new AppError('No Article found with that ID', 404));
   }
 
-  try {
-    res.status(200).render('singleArticle', {
-      title: `Find My Way | ${articleOne.title} `,
-      User: res.locals.user,
+  res.status(200).json({
+    status: 'success',
+    data: {
       data: articleOne,
-    });
-  } catch (err) {
-    return next(
-      new AppError(`${err.response.statusText}`, err.response.status),
-    );
-  }
+    },
+  });
 });
