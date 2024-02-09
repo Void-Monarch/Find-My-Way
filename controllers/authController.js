918db6aef8b409aca4d37c079d9c12e0ada69e7d
Harsh <153886419+Harsh-nodejs@users.noreply.github.com>
2024-02-09 10:53:49 +0530

    hello

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,9 +15,7 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
-    ),
+    expires: new Date(Date.now() + 2592000000),
     httpOnly: true,
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
