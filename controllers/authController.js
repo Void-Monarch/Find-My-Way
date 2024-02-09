411082ab746d7d34823be4ea7a57f8ddd902ae54
Harsh <153886419+Harsh-nodejs@users.noreply.github.com>
2024-02-09 11:04:47 +0530

    Cookie option set to :- samesite : 'lax', instead of lax

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,11 +15,13 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
-    expires: new Date(Date.now() + 2592000000),
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
     httpOnly: true,
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-  cookieOptions.sameSite = true;
+  cookieOptions.sameSite = 'lax';
 
   res.cookie('jwt', token, cookieOptions);
 
